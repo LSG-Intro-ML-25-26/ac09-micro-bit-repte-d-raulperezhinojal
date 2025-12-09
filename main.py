ca291c677ee9d6fed896b221a7e60147f31fd9f9
Raul Pérez Hinojal <raul.perez@gracia.lasalle.cat>
2025-12-09 18:59:50 +0100

    Actualizar pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    if (input.temperature() > 22) {
+        basic.showString("calor ")
+    } else {
+        basic.showString("frio")
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
 basic.forever(function () {
-	
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
+    if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("- - - - - - - - ", 60), music.PlaybackMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    if (input.temperature() > 22) {
+        basic.showString("calor ")
+    } else {
+        basic.showString("frio")
+    }
 })
